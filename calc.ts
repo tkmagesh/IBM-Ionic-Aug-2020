124a581f6f682838f636cb6ac14d426424f55aee
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-11 13:20:21 +0530

    greeter app - initial setup

---
+++ calc.ts
@@ -0,0 +1,24 @@
+/* 
+function add(x : number, y : number) : number {
+    return x + y;
+}
+
+interface Emp {
+    id : number, 
+    name : string,
+    salary : number
+}
+
+var emp : Emp = {
+    id : 100,
+    name : 'Magesh',
+    salary : 1000
+};
+
+console.log(add(10,20)); 
+*/
+
+function add(x,y){
+    return x + y;
+}
+add(10,20);
---
+++ greeter-app/src/main.ts
@@ -0,0 +1,29 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+/* 
+import * as calculator from './calc';
+console.log(calculator); 
+*/
+
+/* import * as calculator from './calc';
+//const add = calculator.add;
+const { add } = calculator; */
+
+/* import { add } from './calc';
+console.log(add(10,20)); */
+
+/* 
+import calc from './calc';
+console.log(calc);
+
+if (environment.production) {
+  enableProdMode();
+} 
+*/
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
