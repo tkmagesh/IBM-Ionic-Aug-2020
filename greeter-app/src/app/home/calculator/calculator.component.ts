db84057e5583605fc5a42cf7f2d2a2e35b50bdf5
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-12 12:57:52 +0530

    constructor injection

--- greeter-app/src/app/home/calculator/calculator.component.ts
+++ greeter-app/src/app/home/calculator/calculator.component.ts
@@ -7,54 +7,17 @@ import { CalculatorService } from "./calculator.service";
     styleUrls : ['./calculator.component.scss']
 })
 export class CalculatorComponent{
-    result : number = 0;
-    _n1 : number = 0;
-    _n2 : number = 0;
+    
 
-    calculatorService : CalculatorService;
+    /*calculatorService : CalculatorService;
 
     constructor(calculatorService: CalculatorService){
         this.calculatorService = calculatorService;
+    } */
+
+    constructor(private calculatorService : CalculatorService){
+
     }
 
-    set n1(value : string){
-        this._n1 = parseFloat(value);
-    }
-
-    set n2(value : string){
-        this._n2 = parseFloat(value);
-    }
-
-    get n1() : string{
-        return String(this._n1) || '';
-    } 
-
-    get n2() : string {
-        return String(this._n2) || '';
-    }
-
-    clearInputs(){
-        /* this.n1 = '';
-        this.n2 = ''; */
-    }
-
-    onAddClick() : void {
-        this.result = this.calculatorService.add(this._n1, this._n2);
-        this.clearInputs();
-    }
-
-    onSubtractClick(): void {
-        this.result = this.calculatorService.subtract(this._n1, this._n2);
-        this.clearInputs();
-    }
-
-    onMultiplyClick(): void {
-        this.result = this.calculatorService.multiply(this._n1, this._n2);
-        this.clearInputs();
-    }
-
-    onDivideClick(): void {
-        this.result = this.calculatorService.divide(this._n1, this._n2);
-        this.clearInputs();
-    }
+    
 }
--- greeter-app/src/app/home/calculator/calculator.service.ts
+++ greeter-app/src/app/home/calculator/calculator.service.ts
@@ -1,17 +1,38 @@
 export class CalculatorService{
-    add(n1 : number, n2 : number) : number {
-        return n1 + n2;
+
+    result: number = 0;
+    _n1: number = 0;
+    _n2: number = 0;
+
+    set n1(value: string) {
+        this._n1 = parseFloat(value);
     }
 
-    subtract(n1: number, n2: number): number {
-        return n1 - n2;
+    set n2(value: string) {
+        this._n2 = parseFloat(value);
     }
 
-    multiply(n1: number, n2: number): number {
-        return n1 * n2;
+    get n1(): string {
+        return String(this._n1) || '';
     }
 
-    divide(n1: number, n2: number): number {
-        return n1 / n2;
+    get n2(): string {
+        return String(this._n2) || '';
+    }
+
+    add() {
+        this.result = this._n1 + this._n2;
+    }
+
+    subtract()  {
+        this.result = this._n1 - this._n2;
+    }
+
+    multiply()  {
+        this.result = this._n1 * this._n2;
+    }
+
+    divide()  {
+        this.result = this._n1 / this._n2;
     }
 }
