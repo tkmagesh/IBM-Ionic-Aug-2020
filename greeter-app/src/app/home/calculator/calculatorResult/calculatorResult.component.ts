65d12620d35929e11753b79bdc243a4a6e2ef918
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-13 09:46:33 +0530

    Communication between components through service

--- greeter-app/src/app/home/calculator/calculatorResult/calculatorResult.component.ts
+++ greeter-app/src/app/home/calculator/calculatorResult/calculatorResult.component.ts
@@ -1,15 +1,13 @@
 import { Component, Input } from "@angular/core";
-
+import { CalculatorService } from '../calculator.service';
 @Component({
     selector : 'app-calculator-result',
     templateUrl : './calculatorResult.component.html',
     styleUrls : ['./calculatorResult.component.scss']
 })
 export class CalculatorResultComponent{
+    constructor(private calculatorService : CalculatorService){
 
-    @Input('data') 
-    result : number = 0;
-    
-    @Input()
-    xyz : string=''
+    }
+   
 }
