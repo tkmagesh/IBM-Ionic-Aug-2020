3839effe395478a8bdca1019bdf741e024dabc71
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-14 10:38:39 +0530

    adding a new product and component life cycle events

--- greeter-app/src/app/home/products/products.component.ts
+++ greeter-app/src/app/home/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import * as moment from "moment";
 import { Product } from "./Product";
@@ -8,40 +8,54 @@ import { ProductsApi } from "./productsApi.service";
     selector : 'app-products',
     template : `
         <h1>Products</h1>
-        <hr>
+        <hr/>
         <ion-input type="text" #txtProductName></ion-input>
         <ion-button (click)="onAddNewClick(txtProductName.value)">Add New</ion-button>
-        <br/>
-        <ion-checkbox (ionChange)="shouldFormat = $event.target.checked"></ion-checkbox>
-        <ion-label>Format</ion-label>
-        <ol>
-            <li *ngFor="let product of products">
-                {{product.name}} - [{{ product.createdAt | elapsed:true }}]
-                <!-- {{product.name}} - [{{ getElapsed(product.createdAt) }}] -->
-            </li>
-        </ol>
+        <h3 *ngIf="isLoading else productsList">Loading...</h3>
+        <ng-template #productsList>
+            <ol>
+                <li *ngFor="let product of products">
+                    {{product.name}} - [{{ product.createdAt | elapsed:true }}]
+                    <!-- {{product.name}} - [{{ getElapsed(product.createdAt) }}] -->
+                </li>
+            </ol>
+        </ng-template>
     `
 })
-export class ProductsComponent{
+export class ProductsComponent implements OnInit, OnDestroy {
 
-    
+    isLoading : boolean = true;
+
     shouldFormat : boolean = false;
 
     products : Product[] = [];
 
     constructor(private productsApi: ProductsApi) {
+        
+    }
+
+    ngOnInit(){
         this.productsApi
             .getAll()
-            .subscribe(products => this.products = products);
+            .subscribe(products => {
+                this.products = products;
+                this.isLoading = false;
+            });
+    }
+
+    ngOnDestroy(){
+
     }
 
     onAddNewClick(newProductName : string){
-        const newProduct : Product = {
+        const newProductData : Product = {
             id : 0,
             name : newProductName,
             createdAt : new Date()
         } 
-        this.products.push(newProduct);
+        this.productsApi
+            .save(newProductData)
+            .subscribe(newProduct => this.products.push(newProduct));
     }
 
     getElapsed(date : Date){
--- greeter-app/src/app/home/products/productsApi.service.ts
+++ greeter-app/src/app/home/products/productsApi.service.ts
@@ -6,12 +6,30 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ProductsApi{
 
+    private serviceEndPoint : string = 'http://localhost:3000/products';
+
     constructor(private http : HttpClient){
 
     }
     getAll() : Observable<Product[]>{
         return this.http
-            .get<Product[]>('http://localhost:3000/products');
+            .get<Product[]>(this.serviceEndPoint);
            
+    }
+    save(productData : Product) : Observable<Product>{
+        if (productData.id === 0){
+            //new product
+            return this.http
+                .post<Product>(this.serviceEndPoint, productData)
+        } else {
+            //existing product
+            return this.http
+                .put<Product>(`${this.serviceEndPoint}/${productData.id}`, productData)
+        }
+    }
+
+    remove(productData : Product) : Observable<any>{
+        return this.http
+            .delete<Product>(`${this.serviceEndPoint}/${productData.id}`);
     }
 }
